f9624cd423d76e602c08b97602e0e55a75cb7bf4
qmoy2510 <qmoy2510@gmail.com>
2025-03-23 15:53:46 +0900

    first

---
+++ Mchome/src/main/java/Mc/Mchome/MchomeApplication.java
@@ -0,0 +1,13 @@
+package Mc.Mchome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MchomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MchomeApplication.class, args);
+	}
+
+}
---
+++ Mchome/src/test/java/Mc/Mchome/MchomeApplicationTests.java
@@ -0,0 +1,13 @@
+package Mc.Mchome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MchomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
